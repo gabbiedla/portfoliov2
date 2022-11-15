71e5132348969e11b15e881da86af1fca1e60db7
Fulcrum <sokatanocw@gmail.com>
2022-11-14 17:11:37 -0800

    3rd two sections

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,18 +1,25 @@
 import './Projects.css';
 import Sql from '../Projects/div2@4x.png';
+import Graph from '../Projects/chart2@4x.png';
+import Logo from '../Projects/Divtest@4x.png';
 
 function Projects() {
   return (
     <div className="proj-container">
-      <div className="sql">
-        <img src={Sql} className="img1" />
-        <p>SQL</p>
-      </div>
-      <div className="ux">
-        <p>UX</p>
-      </div>
-      <div className="dojo">
-        <p>PMDojo</p>
+      <h2>Projects</h2>
+      <div className="sections">
+        <div className="sql">
+          <img src={Sql} className="img1" />
+          <p>SQL</p>
+        </div>
+        <div className="ux">
+          <img src={Graph} className="img2" />
+          <p>UX</p>
+        </div>
+        <div className="dojo">
+          <img src={Logo} className="img3" />
+          <p>PMDojo</p>
+        </div>
       </div>
     </div>
   );
