88f9f50bd0630acf7e0c944fe05f8867d271e3ba
Fulcrum <sokatanocw@gmail.com>
2022-12-17 17:36:07 -0800

    Timeline css

--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -24,6 +24,7 @@ function Timeline() {
             <p className="date">Current</p>
           </div>
         </div>
+
         <div className="container left">
           <div className="content">
             <p>
