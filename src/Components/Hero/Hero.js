bcadcb988d7ac1db78d1d012e673a87dc66a4432
Fulcrum <sokatanocw@gmail.com>
2022-12-21 19:18:19 -0800

    Hero added scroll

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,32 +1,31 @@
 import './Hero.css';
-import Medium from '"/images/medium-circle.png';
+import linkedin from '../Hero/linkedin-circle.png';
+import medium from '../Hero/medium-circle.png';
+import headshot from '../Hero/headshot.jpeg';
 
 function Hero() {
   return (
-    <div>
-      <div className="app-header">
-        {/* <img className="hero-img" src={pmhero} /> */}
-        <div className="top-content">
-          {/* <img src={headshot} className="headshot" alt="headshot" /> */}
-          <h2 className="name">Gabbie De Los Angeles</h2>
-        </div>
-        <div className="bottom-content">
-          {/* <p className="text second-line">Ex-Uber/Bird</p> */}
-
-          <p className="text">
+    <div className="hero-section">
+      <div className="fade">
+        <div className="app-header">
+          <img src={headshot} className="headshot" alt="headshot" />
+          <h1 className="hero-name">Gabbie De Los Angeles</h1>
+          <p className="text ">
             I am a product manager in the B2B space. As a former operations
             leader in micromobility, i have experience in market launches,
             managing image recognition product tools, and launching
             international support teams. I previously worked in hyper-growth
             startups and established corporate environments. I enjoy working on
             web development projects outside of work.
-            <p>Founder of a Toastmasters for LatinX women in tech.</p>
-            <p>Former Bird, Uber, Plantronics (Poly)</p>
           </p>
-        </div>
-        <div className="social">
-          {/* <img src={linkedin} width="30" height="30" />
-          <img src={medium} classname="m-logo" width="25" height="27" /> */}
+          <p className="t2">
+            Founder of a Toastmasters for LatinX women in tech.
+          </p>
+          <p className="t2">Former Bird, Uber, Plantronics (Poly)</p>
+          <div className="social">
+            <img src={linkedin} width="33" height="33" />
+            <img src={medium} classname="m-logo" width="25" height="27" />
+          </div>
         </div>
       </div>
     </div>
