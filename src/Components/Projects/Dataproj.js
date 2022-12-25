3e693a79f60fc69c05847ba01c3afdda16c93eb6
Fulcrum <sokatanocw@gmail.com>
2022-12-24 17:05:21 -0800

    Project design final v1

--- src/Components/Projects/Dataproj.js
+++ src/Components/Projects/Dataproj.js
@@ -1,14 +1,14 @@
 const projs = [
   {
     id: 1,
-    image: './images/Green@3x.png',
+    image: './images/fun@3x.png',
     name: 'Funware',
     description:
       'Funware is a webapp aimed to help leaders of virtual teams increase collaboration and engagement by leveraging fun.',
   },
   {
     id: 2,
-    image: './images/Green@3x.png',
+    image: './images/rds@3x.png',
     name: 'Landing Page Redesign',
     description:
       'Find out how I balanced business needs and technological limitations to achieve a fresh new look for a B2B product landing page',
@@ -16,14 +16,14 @@ const projs = [
 
   {
     id: 3,
-    image: './images/Green@3x.png',
+    image: './images/support@3x.png',
     name: 'International Support Team',
     description: 'Proj description goes here',
   },
 
   {
     id: 4,
-    image: './images/Green@3x.png',
+    image: './images/ml@3x.png',
     name: 'Image Review Tools',
     description:
       'Proj description goes here Proj description goes here Proj description goes here',
