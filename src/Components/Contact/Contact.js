885b1c5fd344ecfee207f0c0376ff71fad76a43d
Fulcrum <sokatanocw@gmail.com>
2022-12-28 20:30:32 -0800

    Contact design responsiveness

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,41 +1,69 @@
 import '../Contact/Contact.css';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-function Contact() {
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_71o9cir',
+        'template_if7f782',
+        form.current,
+        'RVqJcC8XGbk5W6I8h'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset(); //add message sent. make button clickable
+  };
+
   return (
     <div className="contact-Container" id="Contact">
-      <div className="contact-Copy">
-        <h1>Get in touch</h1>
-        <h2>I'd love to hear from you!</h2>
+      <div className="contact-sub-container">
+        <div className="contact-Copy">
+          <h1>Get in touch</h1>
+          <h2>I'd love to hear from you!</h2>
+        </div>
+        <form ref={form} onSubmit={sendEmail} className="form-Section">
+          <label>Full Name</label>
+          <input
+            type="text"
+            placeholder=" Enter your name..."
+            name="name"
+            className="name-Field"
+            tabIndex="1"
+          ></input>
+          <label>Email</label>
+          <input
+            type="text"
+            placeholder=" Enter your email..."
+            name="email"
+            className="email-Field"
+            tabIndex="2"
+          ></input>
+          <label>Message</label>
+          <textarea
+            type="text"
+            placeholder=" Type a message..."
+            name="message"
+            className="email-Field"
+            tabIndex="3"
+          ></textarea>
+          <input className="submit-btn" type="submit" value="Send" />
+          {/* <input type="submit" value="Send" className="submit-btn"></input> */}
+        </form>
       </div>
-      <form className="form-Section">
-        <label>Full Name</label>
-        <input
-          type="text"
-          placeholder="Enter your name..."
-          name="firstName"
-          className="first-Field"
-          tabIndex="1"
-        ></input>
-        <label>Email</label>
-        <input
-          type="text"
-          placeholder="Enter your email..."
-          name="firstName"
-          className="last-Field"
-          tabIndex="1"
-        ></input>
-        <label>Message</label>
-        <input
-          type="text"
-          placeholder="Type a message..."
-          name="firstName"
-          className="email-Field"
-          tabIndex="1"
-        ></input>
-        <input type="submit" value="Send" className="submit-btn"></input>
-      </form>
     </div>
   );
-}
+};
 
 export default Contact;
